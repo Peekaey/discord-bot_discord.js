e57207680ca8bdf85abba631b96f583638e9c527
Dallas <dallasjt31@gmail.com>
2022-07-28 17:53:00 +1000

    removing node_modules

--- index.js
+++ index.js
@@ -1,46 +1,62 @@
 // Require the necessary discord.js classes
 
-const dotenv = require('dotenv').config()
-const { Client, GatewayIntentBits, Collection, StickerPack } = require('discord.js');
-
-const prefix = "?";
-
+const dotenv = require("dotenv").config(); // Required for Discord Token
+const { Client, GatewayIntentBits } = require("discord.js"); // Discord Library
 
 // Create a new client instance
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildVoiceStates
-	],
+  intents: [
+    GatewayIntentBits.Guilds, // Guild Related Things
+    GatewayIntentBits.GuildMembers, // Guild Member Related Things
+    GatewayIntentBits.GuildMessages, // See Guild Messages
+    GatewayIntentBits.MessageContent, // See Message Content
+    GatewayIntentBits.GuildVoiceStates, // See Voice States
+  ],
 });
 
+const { DisTube } = require("distube");
 
+client.DisTube = new DisTube(client, {
+  leaveOnStop: false,
+  emitNewSongOnly: true,
+  emitAddSongWhenCreatingQueue: false,
+  emitAddListWhenCreatingQueue: false,
+});
 
-client.on("messageCreate", message => {
+client.on("messageCreate", (message) => {
+  if (message.author.bot || !message.guild) return;
+  const prefix = "?";
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
-	if(message.content === prefix + "test") {
-		message.channel.send("salut");
-	}
+  if (args.shift().toLowerCase() === "play") {
+    client.DisTube.play(message.member.voice.channel, args.join(" "), {
+      member: message.member,
+      textChannel: message.channel,
+      message,
+    });
+  }
 
-	if (message.content === prefix + "ping") {
-		message.channel.send('Loading data').then (async (msg) =>{
-			msg.delete()
-			message.channel.send(`Ping is ${msg.createdTimestamp - message.createdTimestamp}`);
-		  })
-		  }
+  if (args.shift().toLowerCase() === "ping") {
+	message.channel.send("Loading Data").then(async (msg) => {
+		msg.delete();
+		message.channel.send(
+			`Ping is ${msg.createdTimestamp - message.createdTimestamp}`
+		)
+	});
+  }
 
-})
+});
 
+client.DisTube.on("playSong", (queue, song) => {
+  queue.textChannel.send("NOW PLAYING " + song.name);
+});
 
 // Login to Discord with your client's token
 client
-.login(process.env.DISCORD_TOKEN)
-.then(() => {
-	console.log('Bot has started successfully');
-	console.log('Logged in as',client.user.username);
-	client.user.setActivity(`with ${client.guilds.cache.size} guild(s)`);
-})
-.catch((err) => console.log(err));
-
+  .login(process.env.DISCORD_TOKEN)
+  .then(() => {
+    console.log("Bot has started successfully");
+    console.log("Logged in as", client.user.username);
+    client.user.setActivity(`with ${client.guilds.cache.size} guild(s)`);
+  })
+  .catch((err) => console.log(err));
